4c2304d4727b914290de4294dbef629c498d64a4
Liz Dahlstrom <tanglisha+gitlab@gmail.com>
2023-12-03 21:34:09 -0800

    day 1  part 2

--- day1/day1.py
+++ day1/day1.py
@@ -2,15 +2,44 @@
 
 from curses.ascii import isdigit
 
+digits = dict(
+    one=1,
+    two=2,
+    three=3,
+    four=4,
+    five=5,
+    six=6,
+    seven=7,
+    eight=8,
+    nine=9,
+)
 
 def get_first_digit(input_str: str) -> int:
-    for char in input_str:
-        if char.isdigit():
-            return char
+    for index, item in enumerate(input_str):
+        if item.isdigit():
+            return int(item)
+        
+        for key, value in digits.items():
+            if input_str[index:].startswith(key):
+                return value
+            
+
+def get_last_digit(input_str: str) -> int:
+    start = len(input_str) - 1
+
+    while start >= 0:
+        if input_str[start:start+1].isdigit():
+            return int(input_str[start:start+1])
+        
+        for key, value in digits.items():
+            local_start = start - len(key) + 1
+            if input_str[local_start:].startswith(key):
+                return value
+        start -= 1
 
 def validate(input_line: str) -> int:
     first_digit = get_first_digit(input_line)
-    last_digit = get_first_digit(input_line[::-1])
+    last_digit = get_last_digit(input_line)
     return int(f"{first_digit}{last_digit}")
 
 if "__main__" ==  __name__:
--- day1/test_day1.py
+++ day1/test_day1.py
@@ -1,8 +1,26 @@
-from .day1 import validate
+from .day1 import validate, get_first_digit, get_last_digit
 
 def test_validate_with_known():
     assert validate("1abc2") == 12
     assert validate("pqr3stu8vwx")  == 38
     assert validate("a1b2c3d4e5f") == 15
     assert validate("treb7uchet") == 77
+    assert validate("two1nine") == 29
+    assert validate("eightwothree") == 83
+    assert validate("abcone2threexyz") == 13
+    assert validate("xtwone3four") == 24
+    assert validate("4nineeightseven2") == 42
+    assert validate("zoneight234") == 14
+    assert validate("7pqrstsixteen") == 76
+
+def test_get_first_digit():
+    assert get_first_digit("123") == 1
+    assert get_first_digit("x3x") == 3
+    assert get_first_digit("one23") ==  1
+
+def test_get_last_digit():
+    assert get_last_digit("123") == 3
+    assert get_last_digit("12three") == 3
+    assert get_last_digit("1threex") == 3
+    assert get_last_digit("three") == 3
 
